1540731df12a14f2e36a709e01354f71e5e07021
Aquila Macedo <aquilamacedo@riseup.net>
2025-11-16 11:27:17 -0300

    Define IPC message types and structures for scheduler communication
    Add ShellSchedMsgType, ShellSchedMsgNewProcess and ShellSchedMessage for
    System V message queue communication, and define SHELL_SCHED_CMD_MAX
    (256 bytes) for the command buffer size.

--- shell_sched/core/process.h
+++ shell_sched/core/process.h
@@ -3,11 +3,30 @@
 
 #include "shell_sched/core/common.h"
 
+#include <sys/types.h>
+
+#define SHELL_SCHED_CMD_MAX 256
+
+typedef enum {
+    SHELL_SCHED_MSG_NEW_PROCESS = 1,
+    SHELL_SCHED_MSG_LIST_REQUEST = 2,
+    SHELL_SCHED_MSG_EXIT_REQUEST = 3
+} ShellSchedMsgType;
+
 typedef struct {
     int pid;
     int remaining;
 } ShellSchedProcess;
 
-// TODO
+typedef struct {
+    long mtype; // must be first for System V IPC
+    int priority; // 1 is highest
+    char command[SHELL_SCHED_CMD_MAX];
+} ShellSchedMsgNewProcess;
+
+typedef union {
+    long mtype;
+    ShellSchedMsgNewProcess newProcess;
+} ShellSchedMessage;
 
 #endif
