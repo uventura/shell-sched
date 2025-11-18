548cdd938f14e0f023952e11d182f9fad0bb5aea
uventura <ualivent@gmail.com>
2025-11-18 16:06:59 -0300

    Starting queue

---
+++ shell_sched/utils/queue.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_SCHED_QUEUE_H
+#define SHELL_SCHED_QUEUE_H
+
+typedef struct {
+    
+} ShellSchedQueueNode;
+
+typedef struct {
+    int size;
+    ShellSchedQueueNode* front;
+} ShellSchedQueue;
+
+#endif
