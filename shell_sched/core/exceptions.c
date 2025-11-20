aa646e7b46181cd2cf0ebfe544417eb47b28825e
carollingian <carolina.fdcampos@gmail.com>
2025-11-19 21:45:39 -0300

    nomes

--- shell_sched/core/exceptions.c
+++ shell_sched/core/exceptions.c
@@ -1,3 +1,10 @@
+// gcc version 13.3.0 (Ubuntu 13.3.0-6ubuntu2~24.04)
+// ALUNOS:
+// Aquila Macedo Costa          202021800
+// Carolina Fernandes de Campos 221030830
+// Regina Emy                   190037351
+// Ualiton Ventura da Silva     202033580
+
 #include "shell_sched/core/exceptions.h"
 
 #include <stdio.h>
